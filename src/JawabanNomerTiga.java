9fa576cb080d24aff675b430c0fb3740d51f5a1a
ramsyparker <ramad7287@gmail.com>
2022-11-09 14:53:40 +0700

    ok

--- src/JawabanNomerTiga.java
+++ src/JawabanNomerTiga.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class JawabanNomerTiga {
+public class JawabanNomerTiga {
+    public static void main(String[] args) {
+        System.out.println("Hello kelas 1D");
+    }
 }
